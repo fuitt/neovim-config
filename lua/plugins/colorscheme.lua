73621052f8eef3f26f06c52bfec46e7890370c4b
fuitt <183995717+fuitt@users.noreply.github.com>
2026-05-04 23:51:14 +0900

    feat(snacks): add Neovim version info to header

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,11 @@ return {
   {
     "Mofiqul/vscode.nvim",
     opts = {
+      -- disable_nvimtree_bg = true,
       group_overrides = {
         -- Change the background color of the cursor line and cursor column
+        -- CursorLine = { bg = "#2D2C57" },
+        -- CursorColumn = { bg = "#2D2C57" },
         CursorLine = { bg = "#04395E" },
         CursorColumn = { bg = "#04395E" },
 
@@ -12,7 +15,10 @@ return {
         GitSignsChange = { fg = "#4FC1FF" },
         GitSignsDelete = { fg = "#C74E39" },
 
-        -- Change the colors for Git conflict markers
+        -- Change the background color of the popup menu selection
+        -- PmenuSel = { bg = "#2D2C57" },
+
+        -- Change the color of Git conflict markers
         GitConflictCurrent = { bg = "#264F78" },
         GitConflictIncoming = { bg = "#1B4721" },
 
@@ -24,6 +30,9 @@ return {
         RenderMarkdownH4Bg = { bg = "#1F1F1F" },
         RenderMarkdownH5Bg = { bg = "#1F1F1F" },
         RenderMarkdownH6Bg = { bg = "#1F1F1F" },
+
+        NeovimLogoGreen = { fg = "#54A23D", bold = true },
+        NeovimLogoBlue = { fg = "#3791D4", bold = true },
       },
     },
   },
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,114 @@
+local logo = [[
+                                                                                                    
+     ==          =                                                                                  
+   =====         ===                                                                                
+  =======        +++++                                                       ###                    
+++*========      +++++++                                                                            
+****+=======     +++++++       # #####      ######      ######  ###      ### ###  ### ####   #####  
+******=======    +++++++       ##     ##   #      #   ##      #  ###    ###  ###  ####   #####  ### 
+****** =++=+++   +++++++       #       #  #        # ##        #  ###   ###  ###  ###    ####    ###
+******  ++++++++  ++++++       #       #  #########  #         #  ###  ###   ###  ###    ####    ###
+******   ++++++++ ++++++       #       #  #          #         #   ######    ###  ###    ####    ###
+******     +++++++******       #       #   #          #       ##    ####     ###  ###    ####    ###
+******      +++++++*****       #       #    #######    #######       ###     ###  ###    ####    ###
+######       ++++++++***                                                                            
+ #####        ++++++++                                                                              
+   ###          ++++                                                                                
+     #           **                                                                                 
+                                                                                                    
+]]
+local first = [[
+                                                                                                    
+     ==          =                                                                                  
+   =====         ===                                                                                
+]]
+local v = vim.version()
+local version_string = string.format("%d.%d.%d", v.major, v.minor, v.patch)
+local version_header = [[
+                                                                                          v]] .. version_string
+
 return {
   {
     "folke/snacks.nvim",
     opts = {
+      dashboard = {
+        preset = {
+          -- header = { logo, hl = "SnacksDashboardHeader" },
+          header = {
+            -- { logo, hl = "NeovimLogoGreen" },
+            { "\n" },
+            { first, hl = "NeovimLogoGreen" },
+            { "  =======        +++++                                                       ", hl = "NeovimLogoGreen" },
+            { "###                    " },
+            { "\n" },
+            { "++*", hl = "NeovimLogoBlue" },
+            {
+              "========      +++++++                                                                            ",
+              hl = "NeovimLogoGreen",
+            },
+            { "\n" },
+            { "****+", hl = "NeovimLogoBlue" },
+            { "=======     +++++++       ", hl = "NeovimLogoGreen" },
+            { "# #####      ######      ######  ###      ### ###  ### ####   #####  " },
+            { "\n" },
+            { "******", hl = "NeovimLogoBlue" },
+            { "=======    +++++++       ", hl = "NeovimLogoGreen" },
+            { "##     ##   #      #   ##      #  ###    ###  ###  ####   #####  ### " },
+            { "\n" },
+            { "******", hl = "NeovimLogoBlue" },
+            { " =++=+++   +++++++       ", hl = "NeovimLogoGreen" },
+            { "##     ##   #      #   ##      #  ###    ###  ###  ####   #####  ### " },
+            { "\n" },
+            { "****** ", hl = "NeovimLogoBlue" },
+            { "=++=+++   +++++++       ", hl = "NeovimLogoGreen" },
+            { "#       #  #        # ##        #  ###   ###  ###  ###    ####    ###" },
+            { "\n" },
+            { "******  ", hl = "NeovimLogoBlue" },
+            { "++++++++  ++++++       ", hl = "NeovimLogoGreen" },
+            { "#       #  #########  #         #  ###  ###   ###  ###    ####    ###" },
+            { "\n" },
+            { "******   ", hl = "NeovimLogoBlue" },
+            { "++++++++ ++++++       ", hl = "NeovimLogoGreen" },
+            { "#       #  #          #         #   ######    ###  ###    ####    ###" },
+            { "\n" },
+            { "******     ", hl = "NeovimLogoBlue" },
+            { "+++++++******       ", hl = "NeovimLogoGreen" },
+            { "#       #   #          #       ##    ####     ###  ###    ####    ###" },
+            { "\n" },
+            { "******      ", hl = "NeovimLogoBlue" },
+            { "+++++++*****       ", hl = "NeovimLogoGreen" },
+            { "#       #    #######    #######       ###     ###  ###    ####    ###" },
+            { "\n" },
+            { "######       ", hl = "NeovimLogoBlue" },
+            {
+              "++++++++***                                                                            ",
+              hl = "NeovimLogoGreen",
+            },
+            { "\n" },
+            { " #####        ", hl = "NeovimLogoBlue" },
+            {
+              "++++++++                                                                              ",
+              hl = "NeovimLogoGreen",
+            },
+            { "\n" },
+            { "   ###          ", hl = "NeovimLogoBlue" },
+            {
+              "++++                                                                                ",
+              hl = "NeovimLogoGreen",
+            },
+            { "\n" },
+            { "     #           ", hl = "NeovimLogoBlue" },
+            {
+              "**                                                                                 ",
+              hl = "NeovimLogoGreen",
+            },
+            { "\n" },
+            { "                                                                                                    " },
+            { "\n" },
+            { version_header },
+          },
+        },
+      },
       scroll = { enabled = false },
       terminal = {
         win = {
